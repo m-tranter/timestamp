0352ac27c34afc5fbe67b49be48a3a4d31877814
antiloquax <tranter.m@sky.com>
2024-04-21 19:15:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const myLogger = function (req, _, next) {
@@ -17,30 +17,27 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(myLogger);
 //app.use(express.static('public'));
 
-app.get('/api', function (req, res) {
-  console.log("Empty");
+app.get("/", function (req, res) {
   let myDate = new Date();
   let utc = myDate.toUTCString();
   res.json({ unix: myDate.getTime(), utc: utc });
 });
 
-
-app.get('/api/:date', function (req, res) {
+app.get("/api/:date?", function (req, res) {
   let date = req.params.date;
+  console.log(date);
   let myDate = !date
     ? new Date()
     : new Date(isNum(date) ? parseInt(date) : date);
   let utc = myDate.toUTCString();
-
   res.json(
-    utc === 'Invalid Date'
-      ? { error: 'Invalid date' }
-      : { unix: myDate.getTime(), utc: utc }
+    utc === "Invalid Date"
+      ? { error: "Invalid date" }
+      : { unix: myDate.getTime(), utc: utc },
   );
 });
 
 const port = process.env.PORT ? process.env.PORT : 3001;
 const listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-
+  console.log("Your app is listening on port " + listener.address().port);
 });
